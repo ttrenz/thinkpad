fdc60f453c1951bb5f2e10477a83bb1d9f788bfa
Trenz Thomas <ttrenz@web.de>
2014-02-09 01:18:48 +0100

    Modified thinkpad-battery.sh: Added GPL Info, Author Modified thinkpad-brightness.sh Added GPL Info, Author Modified hinkpad-mute.sh Added GPL Info, Author Added thinkpad-volume.sh Added thinkpad volume handler

--- thinkpad-battery.sh
+++ thinkpad-battery.sh
@@ -1,14 +1,34 @@
 #!/bin/bash 
+# Written by: Thomas Trenz <ttrenz@web.de>
 
-export DISPLAY=":0.0"
-export XAUTHORITY="/home/user/.Xauthority"
-use_smapi(){
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# http://www.gnu.org/copyleft/gpl.html
 
-time_path="/sys/devices/platform/smapi/BAT*/"
-power_path="/sys/devices/platform/smapi/BAT*/"
+if [ -f /usr/share/acpi-support/power-funcs ]
+then
+	. /usr/share/acpi-support/power-funcs
+fi
+getXconsole
+#export XAUTHORITY="/home/user/.Xauthority"
+
+AC_STATUS=$(cat /sys/class/power_supply/AC/online)
+
+use_smapi(){ #{{{
 
 declare power=();
-for file in ${power_path}power_avg
+for file in /sys/devices/platform/smapi/BAT*/power_avg
  do
 	if [ -e $file ]
 	then
@@ -30,7 +50,7 @@ done
 
 
 declare time=();
-for time_file in ${time_path}remaining_running_time
+for time_file in /sys/devices/platform/smapi/BAT*/remaining_running_time
  do
 	if [ -e $time_file ]
 	then
@@ -55,18 +75,19 @@ hours=$(($sum_time/60))
 mins=$(($sum_time-$hours*60))
 POWER=$(echo "scale=2;$sum_power * -1 / 1000" | bc)
 TIME="$hours:`printf %02d $mins`"
-}
+} #}}}
 
-
-use_acpi() {
+use_acpi() { #{{{
 power_file='/sys/class/power_supply/BAT0/power_now'
 raw_power=`cat $power_file`
 POWER=$(echo "scale=2;$raw_power / 1000000" | bc)
 TIME=`acpi | cut -d "," -f 3 | cut -d " " -f 2`
-}
+} #}}}
 
-display_osd() {
-osd_pid='/tmp/osd.pid'
+display_osd() { #{{{
+
+osd_pid='/tmp/osd.pid' # 
+
 if [ -e $osd_pid ];then kill $(cat $osd_pid) 2> /dev/null; fi
 
 if [ -d /sys/devices/platform/smapi ] ; then
@@ -76,14 +97,14 @@ else
 fi
 echo -e "Time remaining : ${TIME} h \nCurrent Power usage: ${POWER} W" | osd_cat -d 2 -A center -o 730 -w --age=1  -c green -l 1 -O 3 -u black -f -xos4-*-*-*-*-*-32-*-*-*-*-*-*-* &
 echo $! > $osd_pid
-}
+} #}}}
 
-display_ac() {
+display_ac() { #{{{
 echo -e "AC Adapter is attached" | osd_cat -d 2 -A center -o 730 -w --age=1  -c green -l 1 -O 3 -u black -f -xos4-*-*-*-*-*-32-*-*-*-*-*-*-* &
-}
+} #}}}
 
 
-if [ $(cat /sys/class/power_supply/AC/online) -eq "0" ]
+if [ $AC_STATUS -eq "0" ]
 then
 display_osd
 else
@@ -91,3 +112,5 @@ display_ac
 fi 
 
 exit 0
+
+# vim:foldmethod=marker
--- thinkpad-volume.sh
+++ thinkpad-volume.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-# Written by Thomas Trenz <ttrenz@web.de>
-
+#!/bin/bash  
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -16,20 +14,55 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 # http://www.gnu.org/copyleft/gpl.html
 
+
 export DISPLAY=":0.0"
 export XAUTHORITY='/home/user/.Xauthority'
 
-level=$(cat /sys/class/backlight/acpi_video0/brightness)    
-let perc=(100*$level)/15
+
+headphone_status() { #{{{
+NUMID=$(amixer contents | grep -iw "name='Headphone Jack'" | cut -d ',' -f1)
+if [ `amixer cget $NUMID | sed -n '/values=[on|off]/p' | cut -d '=' -f 2` == 'values=off' ]
+then
+# Headphone is not plugged
+return 0
+else
+# Headphone is plugged
+return 1
+fi
+} #}}}
+
+case $1 in
+up)
+    mixer_option='5+';;
+down)
+    mixer_option='5-';;
+*)
+    exit 1;;
+esac
+
+
+volume_percent=$(amixer get Master,0 | sed -ne '/^.*Front Left/s/.*\[\(.*\)%\].*/\1/p')
+
+MUTE_STATE=`cat /proc/acpi/ibm/volume | grep -i mute:`
+
+
+
+if [[ -e /tmp/mute.pid ]]
+then
+	echo unmute > /proc/acpi/ibm/volume
+	kill `cat /tmp/mute.pid`
+	amixer set Master,0 $mixer_option > /dev/null
+else
+	amixer set Master,0 $mixer_option > /dev/null
+fi
+
 
 osd_pid='/tmp/osd.pid'
 osd_cmd='osd_cat --f -xos4-*-*-*-*-*-32-*-*-*-*-*-*-* -d 2 -A center -o 700 -l 2 -O 3 -u black -c green  --barmode=slider'
 
 if [[ -f $osd_pid ]]; then kill $(cat $osd_pid) 2> /dev/null; fi
-
-($osd_cmd --percentage=$perc --text="Brightness:$perc % ")  &
+($osd_cmd --percentage=$volume_percent --text="Master Volume:$volume_percent % ") &
 echo $! > $osd_pid
 
-exit 0
 
 # vim:foldmethod=marker
